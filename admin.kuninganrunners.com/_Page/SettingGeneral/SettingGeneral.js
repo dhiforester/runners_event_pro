44d1e61be253cdc9fc883dd523d966381dad837c
dhiforester <dhiforester@gmail.com>
2024-10-07 23:53:28 +0700

    Memperbaiki pengaturan umum

--- admin.kuninganrunners.com/_Page/SettingGeneral/SettingGeneral.js
+++ admin.kuninganrunners.com/_Page/SettingGeneral/SettingGeneral.js
@@ -1,22 +1,187 @@
-//Proses Tambah Akses
-$('#ProsesSettingGeneral').submit(function(){
-    $('#NotifikasiSimpanSettingGeneral').html('<div class="spinner-border text-secondary" role="status"><span class="sr-only"></span></div>');
-    var form = $('#ProsesSettingGeneral')[0];
-    var data = new FormData(form);
-    $.ajax({
-        type 	    : 'POST',
-        url 	    : '_Page/SettingGeneral/ProsesSettingGeneral.php',
-        data 	    :  data,
-        cache       : false,
-        processData : false,
-        contentType : false,
-        enctype     : 'multipart/form-data',
-        success     : function(data){
-            $('#NotifikasiSimpanSettingGeneral').html(data);
-            var NotifikasiSimpanSettingGeneralBerhasil=$('#NotifikasiSimpanSettingGeneralBerhasil').html();
-            if(NotifikasiSimpanSettingGeneralBerhasil=="Success"){
-                window.location.href = "index.php?Page=SettingGeneral";
+$(document).ready(function() {
+    var maxCharsTitle = 20;
+    var maxCharsKeyword = 100;
+    var maxCharsDesc = 500;
+    var maxCharsContact = 20;
+    var maxCharsAlamat = 500;
+    // Fungsi untuk menghitung dan menampilkan jumlah karakter
+    function updateCharCountTitle() {
+        var charCount = $('#title_page').val().length;
+        $('#title_page_length').text(charCount + '/' + maxCharsTitle);
+    }
+    function updateCharCountKeyword() {
+        var charCount = $('#kata_kunci').val().length;
+        $('#keyword_length').text(charCount + '/' + maxCharsKeyword);
+    }
+    function updateCharCountDesc() {
+        var charCount = $('#deskripsi').val().length;
+        $('#description_length').text(charCount + '/' + maxCharsDesc);
+    }
+    function updateCharCountContact() {
+        var charCount = $('#telepon_bisnis').val().length;
+        $('#contact_length').text(charCount + '/' + maxCharsContact);
+    }
+    function updateCharCountAlamat() {
+        var charCount = $('#alamat_bisnis').val().length;
+        $('#alamat_length').text(charCount + '/' + maxCharsAlamat);
+    }
+
+    // Hitung jumlah karakter saat pertama kali halaman dimuat
+    updateCharCountTitle();
+    updateCharCountKeyword();
+    updateCharCountDesc();
+    updateCharCountContact();
+    updateCharCountAlamat();
+    // Fungsi untuk mencegah input jika melebihi batas karakter
+    $('#title_page').on('input', function() {
+        var currentValue = $(this).val();
+        var charCount = currentValue.length;
+        // Cek apakah jumlah karakter melebihi
+        if (charCount > maxCharsTitle) {
+            // Jika melebihi, batasi input
+            $(this).val(currentValue.substring(0, maxCharsTitle));
+        }
+        // Update tampilan jumlah karakter
+        updateCharCountTitle();
+    });
+    $('#kata_kunci').on('input', function() {
+        var currentValue = $(this).val();
+        var charCount = currentValue.length;
+        // Cek apakah jumlah karakter melebihi
+        if (charCount > maxCharsKeyword) {
+            // Jika melebihi, batasi input
+            $(this).val(currentValue.substring(0, maxCharsKeyword));
+        }
+        // Update tampilan jumlah karakter
+        updateCharCountKeyword();
+    });
+    $('#deskripsi').on('input', function() {
+        var currentValue = $(this).val();
+        var charCount = currentValue.length;
+        // Cek apakah jumlah karakter melebihi
+        if (charCount > maxCharsDesc) {
+            // Jika melebihi, batasi input
+            $(this).val(currentValue.substring(0, maxCharsDesc));
+        }
+        // Update tampilan jumlah karakter
+        updateCharCountDesc();
+    });
+    $('#telepon_bisnis').on('input', function() {
+        var currentValue = $(this).val();
+        var charCount = currentValue.length;
+        // Cek apakah jumlah karakter melebihi
+        if (charCount > maxCharsContact) {
+            // Jika melebihi, batasi input
+            $(this).val(currentValue.substring(0, maxCharsContact));
+        }
+        // Hanya mengizinkan angka, simbol +, dan menghapus karakter selain itu
+        var validValue = $(this).val().replace(/[^0-9+]/g, '');
+        $(this).val(validValue);
+        // Update tampilan jumlah karakter
+        updateCharCountContact();
+    });
+    $('#alamat_bisnis').on('input', function() {
+        var currentValue = $(this).val();
+        var charCount = currentValue.length;
+        // Cek apakah jumlah karakter melebihi
+        if (charCount > maxCharsAlamat) {
+            // Jika melebihi, batasi input
+            $(this).val(currentValue.substring(0, maxCharsAlamat));
+        }
+        // Update tampilan jumlah karakter
+        updateCharCountAlamat();
+    });
+    // Fungsi untuk validasi ukuran dan tipe file
+    function validateFile(input, maxSize, allowedTypes, errorElementId) {
+        var file = input.files[0];  // Mendapatkan file dari input
+        var errorMessage = '';
+        if (file) {
+            // Cek ukuran file
+            if (file.size > maxSize) {
+                errorMessage = 'Ukuran file tidak boleh lebih dari ' + (maxSize / 1000000) + ' MB';
+            }
+            // Cek tipe file
+            if (!allowedTypes.includes(file.type)) {
+                errorMessage = 'Tipe file hanya boleh JPG, JPEG, PNG, dan GIF';
             }
         }
+        if (errorMessage) {
+            // Jika ada kesalahan, tampilkan pesan error dan return false
+            $(errorElementId).html(errorMessage).removeClass('text-grayish').show();
+            return false;
+        } else {
+            // Jika valid
+            $(errorElementId).html('File siap diupload').addClass('text-success').show();
+            return true;
+        }
+    }
+    // Validasi Favicon
+    $('#favicon').on('change', function() {
+        var isValid = validateFile(this, 2000000, ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'], '#favicon-error');
+        toggleSubmitButton();
+    });
+    // Validasi Logo
+    $('#logo').on('change', function() {
+        var isValid = validateFile(this, 2000000, ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'], '#logo-error');
+        toggleSubmitButton();
+    });
+    // Fungsi untuk menonaktifkan/menaktifkan tombol submit
+    function toggleSubmitButton() {
+        var faviconValid = $('#favicon-error').text() === ''; // Cek apakah tidak ada error pada Favicon
+        var logoValid = $('#logo-error').text() === ''; // Cek apakah tidak ada error pada Logo
+
+        // Jika kedua file valid, aktifkan tombol submit, jika tidak, nonaktifkan tombol submit
+        if (faviconValid && logoValid) {
+            $('#submitButton').prop('disabled', false);
+        } else {
+            $('#submitButton').prop('disabled', true);
+        }
+    }
+    // Inisialisasi dengan menonaktifkan tombol submit jika error muncul
+    toggleSubmitButton();
+
+    //Proses Simpan Setting
+    $('#ProsesSettingGeneral').on('submit', function(e) {
+        e.preventDefault(); // Mencegah form dari submit secara default
+        // Mengambil data dari form
+        var formData = new FormData(this);
+        // Tombol diubah menjadi "Loading..." saat proses
+        var $submitButton = $('#NotifikasiSimpanSettingGeneral');
+        $submitButton.html('Loading...').prop('disabled', true);
+        // Mengirimkan data melalui AJAX
+        $.ajax({
+            url: '_Page/SettingGeneral/ProsesSettingGeneral.php',
+            method: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            dataType: 'json',
+            success: function(response) {
+                if (response.status === 'success') {
+                    // Jika proses berhasil, reload halaman
+                    window.location.reload();
+                } else {
+                    // Tampilkan notifikasi error jika gagal
+                    Swal.fire(
+                        'Gagal!',
+                        response.message,
+                        'error'
+                    )
+                }
+            },
+            error: function() {
+                // Tampilkan pesan jika terjadi kesalahan pada server
+                Swal.fire(
+                    'Gagal!',
+                    'Terjadi kesalahan pada server, coba lagi nanti.',
+                    'error'
+                )
+            },
+            complete: function() {
+                // Kembalikan tombol ke keadaan semula
+                $submitButton.html('<i class="bi bi-save"></i> Simpan Pengaturan').prop('disabled', false);
+            }
+        });
     });
 });
+
--- admin.kuninganrunners.com/_Page/SettingGeneral/SettingGeneral.php
+++ admin.kuninganrunners.com/_Page/SettingGeneral/SettingGeneral.php
@@ -10,9 +10,9 @@
             <div class="col-md-12">
                 <?php
                     echo '<div class="alert alert-info alert-dismissible fade show" role="alert">';
-                    echo '  Berikut ini adalah halaman pengaturan umum aplikasi.<br>';
-                    echo '  Pada halaman ini anda bisa mengatur properti aplikasi sesuai yang anda inginkan dari mulai judul, deskripsi, informasi kontak dan logo.<br>';
-                    echo '  Periksa kembali pengaturan yang anda gunakan agar aplikasi berjalan dengan baik.<br>';
+                    echo '  Berikut ini adalah halaman pengaturan umum aplikasi.';
+                    echo '  Pada halaman ini anda bisa mengatur properti aplikasi sesuai yang anda inginkan dari mulai judul, deskripsi, informasi kontak dan logo.';
+                    echo '  Periksa kembali pengaturan yang anda gunakan agar aplikasi berjalan dengan baik.';
                     echo '  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
                     echo '</div>';
                 ?>
@@ -28,11 +28,20 @@
                         <div class="card-body">
                             <div class="row mb-3">
                                 <div class="col-md-3">
-                                    <label for="title_page">Judul/Nama Perusahaan</label>
+                                    <label for="title_page">Judul Halaman</label>
                                 </div>
                                 <div class="col-md-9">
-                                    <input type="text" name="title_page" id="title_page" class="form-control" placeholder="Koperasi Andalan Jaya" value="<?php echo "$title_page"; ?>">
-                                    <small>Maksimal 20 karakter</small>
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroupPrepend">
+                                            <small>
+                                                <code class="text text-dark" id="title_page_length">0/20</code>
+                                            </small>
+                                        </span>
+                                        <input type="text" name="title_page" id="title_page" class="form-control" value="<?php echo "$title_page"; ?>">
+                                    </div>
+                                    <small>
+                                        <code class="text text-grayish">Menunjukan nama aplikasi, perusahaan atau lembaga.</code>
+                                    </small>
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -40,8 +49,17 @@
                                     <label for="kata_kunci">Kata Kunci</label>
                                 </div>
                                 <div class="col-md-9">
-                                    <input type="text" name="kata_kunci" id="kata_kunci" class="form-control" value="<?php echo "$kata_kunci"; ?>">
-                                    <small>(Contoh: keyword1, keyword2, keyword3)</small>
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroupPrepend">
+                                            <small>
+                                                <code class="text text-dark" id="keyword_length">0/100</code>
+                                            </small>
+                                        </span>
+                                        <input type="text" name="kata_kunci" id="kata_kunci" class="form-control" value="<?php echo "$kata_kunci"; ?>">
+                                    </div>
+                                    <small>
+                                        <code class="text text-grayish">(Contoh: keyword1, keyword2, keyword3)</code>
+                                    </small>
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -50,31 +68,56 @@
                                 </div>
                                 <div class="col-md-9">
                                     <textarea name="deskripsi" id="deskripsi" cols="30" rows="3" class="form-control"><?php echo "$deskripsi"; ?></textarea>
-                                    <small>Menjelaskan gambaran umum aplikasi ini</small>
+                                    <small>
+                                        <code class="text text-grayish">Menjelaskan gambaran umum aplikasi</code>
+                                        <code class="text text-dark" id="description_length">(0/500)</code>
+                                    </small>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col-md-3">
-                                    <label for="alamat_bisnis">Alamat/Kantor</label>
+                                    <label for="email">Email</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroupPrepend">
+                                            <i class="bi bi-envelope"></i>
+                                        </span>
+                                        <input type="email" name="email_bisnis" id="email_bisnis" class="form-control" placeholder="email@domain.com" value="<?php echo "$email_bisnis"; ?>">
+                                    </div>
+                                    <small>
+                                        <code class="text text-grayish">Email resmi yang dapat dihubungi</code>
+                                    </small>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-3">
+                                    <label for="telepon_bisnis">Kontak</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroupPrepend">
+                                            <small>
+                                                <code class="text text-dark" id="contact_length">0/20</code>
+                                            </small>
+                                        </span>
+                                        <input type="text" name="telepon_bisnis" id="telepon_bisnis" class="form-control" placeholder="+62" value="<?php echo "$telepon_bisnis"; ?>">
+                                    </div>
+                                    <small>
+                                        <code class="text text-grayish">Nomor kontak/telepon administrator pengelola aplikasi</code>
+                                    </small>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-3">
+                                    <label for="alamat_bisnis">Alamat</label>
                                 </div>
                                 <div class="col-md-9">
                                     <textarea name="alamat_bisnis" id="alamat_bisnis" cols="30" rows="3" class="form-control"><?php echo "$alamat_bisnis"; ?></textarea>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-3">
-                                    <label for="email">Email Perusahaan</label>
-                                </div>
-                                <div class="col-md-9">
-                                    <input type="email" name="email_bisnis" id="email_bisnis" class="form-control" placeholder="email@domain.com" value="<?php echo "$email_bisnis"; ?>">
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-3">
-                                    <label for="telepon_bisnis">Nomor Telepon</label>
-                                </div>
-                                <div class="col-md-9">
-                                    <input type="text" name="telepon_bisnis" id="telepon_bisnis" class="form-control" placeholder="+62" value="<?php echo "$telepon_bisnis"; ?>">
+                                    <small>
+                                        <code class="text text-grayish">Alamat kantor/operasional</code>
+                                        <code class="text text-dark" id="alamat_length">(0/500)</code>
+                                    </small>
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -82,14 +125,18 @@
                                     <label for="favicon">File Favicon</label>
                                 </div>
                                 <div class="col-md-9">
-                                    <input type="file" name="favicon" id="favicon" class="form-control">
-                                    <small>
-                                        Maksimal File 2 Mb (JPG, JPEG, PNG and GIF)
+                                    <div class="input-group">
+                                        <input type="file" name="favicon" id="favicon" class="form-control">
                                         <?php
                                             if(!empty($favicon)){
-                                                echo '<a href="assets/img/'.$favicon.'" target="_blank">View Image Here</a>';
+                                                echo '<span class="input-group-text" id="inputGroupPrepend">';
+                                                echo '<a href="assets/img/'.$favicon.'" target="_blank"><i class="bi bi-image"></i></a>';
+                                                echo '</span>';
                                             }
                                         ?>
+                                    </div>
+                                    <small>
+                                        <code class="text text-grayish" id="favicon-error">Maksimal Ukuran File 2 Mb (Format: JPG, JPEG, PNG and GIF)</code>
                                     </small>
                                 </div>
                             </div>
@@ -98,14 +145,18 @@
                                     <label for="logo">Logo Image</label>
                                 </div>
                                 <div class="col-md-9">
-                                    <input type="file" name="logo" id="logo" class="form-control">
-                                    <small>
-                                        Maksimal File 2 Mb (JPG, JPEG, PNG and GIF)
+                                    <div class="input-group">
+                                        <input type="file" name="logo" id="logo" class="form-control">
                                         <?php
                                             if(!empty($logo)){
-                                                echo '<a href="assets/img/'.$logo.'" target="_blank">View Image Here</a>';
+                                                echo '<span class="input-group-text" id="inputGroupPrepend">';
+                                                echo '<a href="assets/img/'.$logo.'" target="_blank"><i class="bi bi-image"></i></a>';
+                                                echo '</span>';
                                             }
                                         ?>
+                                    </div>
+                                    <small>
+                                        <code class="text text-grayish" id="logo-error">Maksimal Ukuran File 2 Mb (Format: JPG, JPEG, PNG and GIF)</code>
                                     </small>
                                 </div>
                             </div>
@@ -114,19 +165,24 @@
                                     <label for="base_url">Base URL</label>
                                 </div>
                                 <div class="col-md-9">
-                                    <input type="text" name="base_url" id="base_url" class="form-control" placeholder="https://" value="<?php echo "$base_url"; ?>">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroupPrepend">
+                                            <i class="bi bi-globe"></i>
+                                        </span>
+                                        <input type="url" name="base_url" id="base_url" class="form-control" placeholder="https://" value="<?php echo "$base_url"; ?>">
+                                    </div>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col-md-3">
                                 </div>
-                                <div class="col-md-9 text-right" id="NotifikasiSimpanSettingGeneral">
+                                <div class="col-md-9 text-right">
                                     <small class="text-dark">Pastikan pengaturan yang anda gunakan sudah sesuai.</small>
                                 </div>
                             </div>
                         </div>
                         <div class="card-footer">
-                            <button type="submit" class="btn btn-md btn-primary btn-rounded">
+                            <button type="submit" class="btn btn-md btn-primary btn-rounded" id="NotifikasiSimpanSettingGeneral">
                                 <i class="bi bi-save"></i> Simpan Pengaturan
                             </button>
                         </div>
